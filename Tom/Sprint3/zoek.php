76b097fbabdfc6b51388636acb9b3524fa6b2a64
Tom-Coupe <tcphonehome@gmail.com>
2018-11-26 13:30:39 +0100

    tom commit

---
+++ Tom/Sprint3/zoek.php
@@ -0,0 +1,105 @@
+<?php
+include 'session.php';
+include 'connection.php';
+include 'functions.php';
+$zoek = filter_input(INPUT_GET, "zoek", FILTER_SANITIZE_STRING);
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <link rel="stylesheet" type="text/css" href="categorie-style.css">
+        <link rel="stylesheet" type="text/css" href="style.css">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+        <title></title>
+
+    </head>
+    <body>
+        <!-- Hier wordt de navbar opgevraagd -->
+        <?php $zoekPagina = TRUE;
+        include 'navbar.php';
+        ?>
+
+        <div class="container">
+
+            <?php
+            try {
+                $search = $_GET['zoek'];
+                //Hier worden artikelen opgezocht waarbij de naam, de tags of zoekdetails lijken op een bepaalde zoekterm
+                $query = $conn->query("SELECT * FROM stockitems WHERE StockItemName LIKE '%" . $search . "%' OR SearchDetails LIKE '%" . $search . "%' OR Tags LIKE '%" . $search . "%'");
+                $count = $query->rowCount();
+                $artikelNaamTweedeKeer = '';
+                while ($row = $query->fetch()) {
+                    $artikelNaam = $row["StockItemName"]; 
+                    $artikelID = $row["StockItemID"];
+                    $artikelPrijs = $row["RecommendedRetailPrice"];
+                    $artikelMaat = $row["Size"];
+                    $Small = like_match('%S', $artikelMaat);
+                    $Medium = like_match('M', $artikelMaat);
+                    $Large = like_match('%L', $artikelMaat);
+                    $LengteEen = like_match('%.%m', $artikelMaat);
+                    $LengteTwee = like_match('%0m', $artikelMaat);
+                    $LengteDrie = like_match('%x%m', $artikelMaat);                    
+                    //Hier wordt gekeken of er tussen de artikelen iets zit met een maatje S, M of L. Hiervan wordt er maar één weergegeven tussen de resultaten. De maat kan dan pas op de artikelpagina zelf worden gekozen.
+                    if ($LengteEen OR $LengteTwee OR $LengteDrie) {
+                        $artikelNaam = str_replace($artikelMaat, '', $artikelNaam);
+                        $artikelNaam = $artikelNaam;
+                        if ($artikelNaam != $artikelNaamTweedeKeer) {
+                            $artikelNaamTweedeKeer = $artikelNaam;
+                        } else {
+                            goto a;
+                        }
+                    }
+                    if ($Small OR $Medium OR $Large) {
+                        $artikelNaam = str_replace(") " . $artikelMaat, '', $artikelNaam);
+                        $artikelNaam = $artikelNaam . ")";
+                        if ($artikelNaam != $artikelNaamTweedeKeer) {
+                            $artikelNaamTweedeKeer = $artikelNaam;
+                        } else {
+                            goto a;
+                        }
+                    }
+                    if (isset($artikelID)) {
+                        ?>
+                        <a href="artikel.php?artikelid=<?php print($artikelID."&maatselected=FALSE"); ?>" class="artikelTekst">
+                        <div class="row">
+                            <div class="col-lg-4 col-md-4 col-sm-12">
+                                <?php $fotoID = rand(1, 17); ?>
+                                <img src="artikelFoto/<?php print($fotoID); ?>.jpg"  class="artikelImg">
+                            </div>
+                            <div class="col-lg-4 col-md-4 col-sm-12">
+                                <h5><?php print($artikelNaam); ?></h5>
+                            </div>
+                            <div class="col-lg-4 col-md-4 col-sm-12">
+                                <?php print("<h3 class='artikelPrijs'>€" . $artikelPrijs . "</h3>"); ?>
+                            </div>
+                            <hr style="width:80%">
+                        </div>
+                        </a>
+
+
+                        <?php
+                        a:
+                    }
+                }
+                if ($count == 0) {
+                    echo 'Geen zoekresultaten gevonden voor: \'' . $zoek . "'";
+                }
+            } catch (PDOException $e) {
+                print "Error!: " . $e->getMessage() . "<br/>";
+                die();
+            }
+            ?>
+        </div>
+
+
+
+
+
+        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+
+    </body>
+</html>
