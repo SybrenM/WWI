60c3cd51abbd25e131c8a0d4047fbe12ba9588e4
SybrenM <Sybren.moes@live.nl>
2018-12-10 11:27:50 +0100

    verwijder

--- Oplevering MkII/Code/Website GOEIE INCL FUCKUP VAN TOM/verwijderArtikel.php
+++ Oplevering MkII/Code/Website GOEIE INCL FUCKUP VAN TOM/verwijderArtikel.php
@@ -5,12 +5,18 @@ $aantal = filter_input(INPUT_GET, "aantal", FILTER_SANITIZE_STRING);
 
 
 include 'connection.php';
+//Query controleert de artikelitem die verwijdert moet worden,
 $row = $conn->query("SELECT * FROM stockitems WHERE StockItemID = ".$id);
     while ($artikel = $row->fetch()) {
 	foreach($_SESSION['winkelwagen'] as $key => $value){
+		//Als de stockitemid van winkelwagen hetzelfde is als stockitemid uit sessie winkelwagen
+
 					if($value == $artikel["StockItemID"]){
+	//Zet alle keys van sessie array "winkelwagen" in variable $keys
 
 			$keys = array_keys($_SESSION['winkelwagen']);
+				//Als de key van sessie array "winkelwagen" met als waarde variabel $i van winkelwagen.php overeen komt met key van sessie array "winkelwagen"
+
 				if($keys[$aantal] == $key){
 				echo "Aantal :". $_SESSION['aantal'][$key];
 				unset ( $_SESSION['aantal'][$key]);
